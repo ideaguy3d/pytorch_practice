cf51dc927aaa6aef7b63fd9ede490fd1bcef7e75
Julius Hernandez <julius_hernandez@apple.com>
2024-02-11 22:05:16 -0700

    encoder only transformer

--- transformer_one.py
+++ transformer_one.py
@@ -20,6 +20,7 @@
 # %%
 import torch 
 import math 
+# import torch.nn.functional as F 
 from torch import nn  
 
 print(torch.__version__)
@@ -81,5 +82,147 @@ math.log(10.)
 
 # %%
 2 ** 3
+
+
+# %% [markdown]
+# # Multi-headed Attention Class 
+
+# %%
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embedding_size, num_heads):
+        """
+        embedding_size: embedding dimension size, model dimensions 
+        """
+        super(MultiHeadAttention, self).__init__()
+        self.embedding_size = embedding_size
+        self.num_heads = num_heads
+        self.head_dim = embedding_size // num_heads
+        
+        self.query_linear = nn.Linear(embedding_size, embedding_size)
+        self.key_linear = nn.Linear(embedding_size, embedding_size)
+        self.value_linear = nn.Linear(embedding_size, embedding_size)
+        self.output_linear = nn.Linear(embedding_size, embedding_size)
+        
+    def split_heads(self, x, batch_size):
+        x = x.view(batch_size, -1, self.num_heads, self.head_dim) 
+        return x.permute(0, 2, 1, 3).contiguous().view(batch_size * self.num_heads, -1, self.head_dim)
+    
+    @staticmethod
+    def compute_attention(query, key, mask=None):
+        scores = torch.matmul(query, key.permute(1,2,0))
+        if mask is not None:
+            scores = scores.masked_fill(mask==0, float("-1e9"))
+        attention_weights = torch.nn.functional.softmax(scores, dim=-1)
+        return attention_weights 
+    
+    def forward(self, query, key, value, mask=None):
+        batch_size = query.size(0)
+        
+        query = self.split_heads(self.query_linear(query), batch_size)
+        key = self.split_heads(self.key_linear(key), batch_size)
+        value = self.split_heads(self.value_linear(value), batch_size)
+        
+        attention_weights = self.compute_attention(query, key, mask)
+        
+        output = torch.matmul(attention_weights, value)
+        output = (output
+                  .view(batch_size, self.num_heads, -1, self.head_dim)
+                  .permute(0, 2, 1, 3)
+                  .contiguous()
+                  .view(batch_size, -1, self.embedding_size))
+        
+        return self.output_linear(output)
+
+
+# %%
+
+# %%
+7 // 2
+
+
+# %% [markdown]
+# # Encoder Only Transformer Class
+
+# %%
+class FeedForwardSublayer(nn.Module):
+    def __init__(self, model_dimensions, dim_between_layers):
+        super(FeedForwardSublayer, self).__init__()
+        self.fc1 = nn.Linear(model_dimensions, dim_between_layers)
+        self.fc2 = nn.Linear(dim_between_layers, model_dimensions)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
+    
+
+class EncoderLayer(nn.Module):
+    def __init__(self, model_dimensions, num_heads, dim_between_layers, dropout):
+        """
+        Args:
+            model_dimensions: d_model
+            num_heads: 
+            dim_between_layers: d_ff
+            dropout: 
+        """
+        super(EncoderLayer, self).__init__()
+        self.self_attn = MultiHeadAttention(model_dimensions, num_heads)
+        self.feed_forward = FeedForwardSublayer(model_dimensions, dim_between_layers)
+        self.norm1 = nn.LayerNorm(model_dimensions)
+        self.norm2 = nn.LayerNorm(model_dimensions)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x, mask):
+        attn_output = self.self_attn(x, x, x, mask) # ??? 
+        x = self.norm1(x + self.dropout(attn_output))
+        ff_output = self.feed_forward(x)
+        x = self.norm2(x + self.dropout(ff_output))
+        return x 
+    
+class TransformerEncoder(nn.Module):
+    def __init__(self, vocab_size, d_model, num_layers, num_heads, d_ff, dropout, max_seq_len):
+        """
+        Args:
+            vocab_size: 
+            d_model: model_dimensions
+            num_layers: 
+            num_heads: 
+            d_ff: dim_between_layers
+            dropout: 
+            max_seq_len: 
+        """
+        super(TransformerEncoder, self).__init__() 
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.positional_encoding = PositionalEncoder(embedding_size=d_model, max_seq_len=max_seq_len)
+        self.layers = nn.ModuleList([
+            EncoderLayer(model_dimensions=d_model, num_heads=num_heads, dim_between_layers=d_ff, dropout=dropout)
+            for _ in range(num_layers)
+        ])
+        
+    def forward(self, x, mask):
+        x = self.embedding(x)
+        x = self.positional_encoding(x)
+        for layer in self.layers:
+            x = layer(x, mask)
+        return x 
+    
+
+class ClassifierHead(nn.Module):
+    def __init__(self, d_model, num_classes):
+        super(ClassifierHead, self).__init__()
+        self.fc = nn.Linear(d_model, num_classes)
+        
+    def forward(self, x):
+        logits = self.fc(x)
+        return torch.nn.functional.log_softmax(logits, dim=-1)
+
+class RegressionHead(nn.Module):
+    def __init__(self, d_model, output_dim):
+        super(RegressionHead, self).__init__()
+        self.fc = nn.Linear(d_model, output_dim)
+        
+    def forward(self, x):
+        return self.fc(x) 
+
+# %%
 
 # %%
